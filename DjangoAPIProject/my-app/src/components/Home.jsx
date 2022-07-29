6fd14fa239b6540197aca729784e843d61dca2b1
rickygeetchi <rickygeetchi@gmail.com>
2022-07-28 23:26:47 -0500

    more beautifying

--- DjangoAPIProject/my-app/src/components/Home.jsx
+++ DjangoAPIProject/my-app/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { ReactComponent as HomeSVG } from "../svg/home.svg";
+import { ReactComponent as EmployeeSVG } from "../svg/employees.svg";
+import { ReactComponent as DepartmentSVG } from "../svg/department.svg";
+import { Link } from 'react-router-dom';
+import '../index.css';
 
 const Home = () => {
   return (
-    <div className="display-3 d-flex justify-content-center mt-5 mb-n3"><HomeSVG/></div>
+    <React.Fragment>
+    <div className="display-3 d-flex justify-content-center mt-5 mb-n3"><Link to={'/'}><HomeSVG/></Link></div>
+    <span className='bold d-flex justify-content-center mt-6 mb-n6'>Choose One</span>
+    <div className="icon display-3 d-flex justify-content-center mt-5 mb-n3"><Link to={'/department'}><DepartmentSVG/></Link></div>
+    <div className="display-3 d-flex justify-content-center mt-5 mb-n3"><Link to={'/employees'}><EmployeeSVG/></Link></div>
+    </React.Fragment>
   )
 }
 
